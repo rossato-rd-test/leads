526d9cef87525db365220668ea68d1f07f2d042c
Thiago Rossato <rossato.rd.test01@gmail.com>
2015-08-27 05:43:12 -0300

    A better table style for leads and some buttons.

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -1,6 +1,6 @@
 class LeadsController < ApplicationController
   before_action :set_lead, only: [:show, :edit, :update, :destroy]
-
+  before_action :setup_salesforce, except: [:new]
   # GET /leads
   # GET /leads.json
   def index
@@ -28,11 +28,10 @@ class LeadsController < ApplicationController
 
     respond_to do |format|
       if @lead.save
+        sync_contact(@lead)
         format.html { redirect_to @lead, notice: 'Lead was successfully created.' }
-        format.json { render :show, status: :created, location: @lead }
       else
         format.html { render :new }
-        format.json { render json: @lead.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -42,11 +41,10 @@ class LeadsController < ApplicationController
   def update
     respond_to do |format|
       if @lead.update(lead_params)
+        sync_contact(@lead)
         format.html { redirect_to @lead, notice: 'Lead was successfully updated.' }
-        format.json { render :show, status: :ok, location: @lead }
       else
         format.html { render :edit }
-        format.json { render json: @lead.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -57,18 +55,50 @@ class LeadsController < ApplicationController
     @lead.destroy
     respond_to do |format|
       format.html { redirect_to leads_url, notice: 'Lead was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_lead
-      @lead = Lead.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def lead_params
-      params.require(:lead).permit(:name, :last_name, :email, :company, :job_title, :phone, :website, :salesforce_id, :salesforce_updated_at)
+  # Use callbacks to share common setup or constraints between actions.
+  def set_lead
+    @lead = Lead.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def lead_params
+    params.require(:lead).permit(:name, :last_name, :email, :company, :job_title, :phone, :website, :salesforce_id, :salesforce_updated_at)
+  end
+
+  # Setup Salesforce connector
+  def setup_salesforce
+    if(@salesforce == nil)
+      config = Admin::Salesforce.first
+      @salesforce = Databasedotcom::Client.new(:client_id => config.client_id, :client_secret => config.client_secret)
+      @salesforce.authenticate(:username => config.username, :password => config.password + config.security_token)
     end
+  end
+
+  # Sync contact
+  def sync_contact (lead)
+    setup_salesforce
+
+    params = {"FirstName" => lead.name,
+      "LastName" => lead.last_name,
+      "Email" => lead.email,
+      "Phone" => lead.phone,
+      "Title" => lead.job_title,
+      "OwnerId" => @salesforce.user_id}
+
+    if lead.salesforce_id != nil && @salesforce.find("Contact", lead.salesforce_id)
+      @salesforce.update("Contact", lead.salesforce_id, params)
+    else
+      c = @salesforce.create("Contact", params)
+    c.save()
+    lead.salesforce_id = c.Id
+    end
+    lead.salesforce_updated_at = Date.new()
+    lead.save()
+  end
+
 end
--- app/views/leads/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @lead, :id, :name, :last_name, :email, :company, :job_title, :phone, :website, :salesforce_id, :salesforce_updated_at, :created_at, :updated_at
