89c806acb381ba47f295256f4ae4203a9b03fa8e
Thiago Rossato <rossato.rd.test01@gmail.com>
2015-08-27 02:09:53 -0300

    Salesforce configuration page.

--- app/controllers/admin/salesforces_controller.rb
+++ app/controllers/admin/salesforces_controller.rb
@@ -1,14 +1,18 @@
 class Admin::SalesforcesController < ApplicationController
   before_action :set_admin_salesforce, only: [:show, :edit, :update, :destroy]
-
   # GET /admin/salesforces
-  # GET /admin/salesforces.json
   def index
     @admin_salesforces = Admin::Salesforce.all
+
+    if(@admin_salesforces.count == 0)
+     redirect_to action: "new"
+    else
+      redirect_to action: "edit", id: @admin_salesforces[0].id
+    end
+
   end
 
   # GET /admin/salesforces/1
-  # GET /admin/salesforces/1.json
   def show
   end
 
@@ -22,53 +26,40 @@ class Admin::SalesforcesController < ApplicationController
   end
 
   # POST /admin/salesforces
-  # POST /admin/salesforces.json
   def create
     @admin_salesforce = Admin::Salesforce.new(admin_salesforce_params)
 
-    respond_to do |format|
-      if @admin_salesforce.save
-        format.html { redirect_to @admin_salesforce, notice: 'Salesforce was successfully created.' }
-        format.json { render :show, status: :created, location: @admin_salesforce }
-      else
-        format.html { render :new }
-        format.json { render json: @admin_salesforce.errors, status: :unprocessable_entity }
-      end
+    if @admin_salesforce.save
+      redirect_to @admin_salesforce, notice: 'Salesforce was successfully created.'
+    else
+      render :new
     end
   end
 
   # PATCH/PUT /admin/salesforces/1
-  # PATCH/PUT /admin/salesforces/1.json
   def update
-    respond_to do |format|
-      if @admin_salesforce.update(admin_salesforce_params)
-        format.html { redirect_to @admin_salesforce, notice: 'Salesforce was successfully updated.' }
-        format.json { render :show, status: :ok, location: @admin_salesforce }
-      else
-        format.html { render :edit }
-        format.json { render json: @admin_salesforce.errors, status: :unprocessable_entity }
-      end
+    if @admin_salesforce.update(admin_salesforce_params)
+      redirect_to @admin_salesforce, notice: 'Salesforce was successfully updated.'
+    else
+      render :edit
     end
   end
 
   # DELETE /admin/salesforces/1
-  # DELETE /admin/salesforces/1.json
   def destroy
     @admin_salesforce.destroy
-    respond_to do |format|
-      format.html { redirect_to admin_salesforces_url, notice: 'Salesforce was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to admin_salesforces_url, notice: 'Salesforce was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_admin_salesforce
-      @admin_salesforce = Admin::Salesforce.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def admin_salesforce_params
-      params.require(:admin_salesforce).permit(:username, :password, :client_id, :client_secret, :security_token)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_admin_salesforce
+    @admin_salesforce = Admin::Salesforce.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def admin_salesforce_params
+    params.require(:admin_salesforce).permit(:username, :password, :client_id, :client_secret, :security_token)
+  end
 end
--- app/views/admin/salesforces/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @admin_salesforce, :id, :username, :password, :client_id, :client_secret, :security_token, :created_at, :updated_at
